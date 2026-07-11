eb1391e43f9910c99ccc817f4273229b421e34b9
DenzelWahyudi <denzelmark8400@gmail.com>
2026-07-11 10:52:01 +0700

    feat: add schedule page

--- src/components/Heading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Text } from 'react-native';
-export default function Heading({ children }: { children: React.ReactNode }){
-    return (
-        <Text className="text-4xl font-bold text-white">{children}</Text>
-    )
-}
